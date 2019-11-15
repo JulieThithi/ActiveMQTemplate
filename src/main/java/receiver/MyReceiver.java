8238ad7a597e53395b2e4fbf1ac11bdd49e49a74
JulieThithi <57801718+JulieThithi@users.noreply.github.com>
2019-11-15 14:57:22 +0100

    Add files via upload

--- src/main/java/receiver/MyReceiver.java
+++ src/main/java/receiver/MyReceiver.java
@@ -9,7 +9,11 @@ import javax.jms.QueueSender;
 import javax.jms.QueueSession;
 import javax.jms.Session;
 import javax.jms.TextMessage;
-
+import javax.jms.Topic;
+import javax.jms.TopicConnection;
+import javax.jms.TopicConnectionFactory;
+import javax.jms.TopicSession;
+import javax.jms.TopicSubscriber;
 import javax.jms.Queue;
 import javax.jms.QueueConnectionFactory;
 import org.springframework.context.ApplicationContext;
@@ -21,15 +25,31 @@ public class MyReceiver {
 		try{
 			
 			ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContextJMS.xml");
-			QueueConnectionFactory factory = (QueueConnectionFactory) applicationContext.getBean("connectionFactory");
+			//QueueConnectionFactory factory = (QueueConnectionFactory) applicationContext.getBean("connectionFactory");
+			TopicConnectionFactory factory = (TopicConnectionFactory) applicationContext.getBean("connectionFactory");
 			
-			Queue queue = (Queue) applicationContext.getBean("queue");
+			//Queue queue = (Queue) applicationContext.getBean("queue");
+			Topic topic = (Topic) applicationContext.getBean("topic");
 			
 			// Create a connection. See https://docs.oracle.com/javaee/7/api/javax/jms/package-summary.html
-			// Open a session	
+			//QueueConnection connection = factory.createQueueConnection() ;
+			TopicConnection connection = factory.createTopicConnection() ;
+			
+			// Open a session
+			//QueueSession session = connection.createQueueSession(true, Session.AUTO_ACKNOWLEDGE) ;
+			TopicSession session = connection.createTopicSession(true, Session.AUTO_ACKNOWLEDGE) ;
+			
 			// start the connection	
+			connection.start();
+			
 			// Create a receive	
+			//QueueReceiver receiver = session.createReceiver(queue);
+			TopicSubscriber receiver = session.createSubscriber(topic) ;
+			
 			// Receive the message
+			Message m = receiver.receive();
+			
+			System.out.println(m + "JULIE ");
 			
 		}catch(Exception e){
 			e.printStackTrace();
--- src/main/java/sender/MySender.java
+++ src/main/java/sender/MySender.java
@@ -8,10 +8,18 @@ import javax.jms.QueueSender;
 import javax.jms.QueueSession;
 import javax.jms.Session;
 import javax.jms.TextMessage;
+import javax.jms.Topic;
+import javax.jms.TopicConnection;
+import javax.jms.TopicConnectionFactory;
+import javax.jms.TopicPublisher;
+import javax.jms.TopicSession;
 import javax.jms.QueueConnectionFactory;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+//Is a queue able to receive message from many senders? Yes !
+//Is a queue able to send message to many receivers? No ! (but a Topic can)
 
 public class MySender {
 
@@ -20,18 +28,38 @@ public class MySender {
 		try{
 			
 			ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContextJMS.xml");
-			QueueConnectionFactory factory = (QueueConnectionFactory) applicationContext.getBean("connectionFactory");
+			//QueueConnectionFactory factory = (QueueConnectionFactory) applicationContext.getBean("connectionFactory");
+			TopicConnectionFactory factory = (TopicConnectionFactory) applicationContext.getBean("connectionFactory");
 			
-			Queue queue = (Queue) applicationContext.getBean("queue");
+			//Queue queue = (Queue) applicationContext.getBean("queue");
+			Topic topic = (Topic) applicationContext.getBean("topic");
 			
 			// Create a connection. See https://docs.oracle.com/javaee/7/api/javax/jms/package-summary.html	
+			//QueueConnection connection = factory.createQueueConnection() ;
+			TopicConnection connection = factory.createTopicConnection() ;
+			
 			// Open a session without transaction and acknowledge automatic
+			//QueueSession session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE) ;
+			TopicSession session = connection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE) ;
+			
 			// Start the connection
+			connection.start();
+			
 			// Create a sender
+			//QueueSender sender = session.createSender(queue) ;
+			TopicPublisher sender = session.createPublisher(topic) ;
+			
 			// Create a message
+			TextMessage m = session.createTextMessage("yo");
+			
 			// Send the message
+			sender.send( m );
+			
 			// Close the session
+			session.close();
+			
 			// Close the connection
+			connection.close();
 
 		}catch(Exception e){
 			e.printStackTrace();
